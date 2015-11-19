47b560e4b68deb6c2094abf139fbf543281a3703
sangv <sang@driveclutch.com>
2015-11-19 14:19:03 -0500

    Version changes

--- java/attributes/customize.rb
+++ java/attributes/customize.rb
@@ -1,1 +1,3 @@
-normal['java']['jdk_version'] = '7'
+normal['java']['install_flavor'] = 'oracle'
+normal['java']['jdk_version'] = '8'
+normal['java']['oracle']['accept_oracle_download_terms'] = 'true'

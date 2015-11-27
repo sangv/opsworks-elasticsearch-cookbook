0807f5f44b415736fa30fdf4e30d39e1289111bc
sangv <sang@driveclutch.com>
2015-11-27 14:32:39 -0500

    Versions

--- elasticsearch/attributes/customize.rb
+++ elasticsearch/attributes/customize.rb
@@ -1,3 +1,3 @@
-normal[:elasticsearch][:version] = '1.7.0'
+normal[:elasticsearch][:version] = '1.7.3'
 normal[:elasticsearch][:plugins]['elasticsearch/elasticsearch-cloud-aws'][:version] = '2.7.0' # Must correpond to the proper Elasticsearch version. See the elasticsearch-cloud-aws repo for more info.
 normal[:elasticsearch][:cluster][:name] = 'prod_cluster'  # Change to whatever you want.
